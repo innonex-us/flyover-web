060377f9637926ea8a770c361cab2d4182a6c480
Md Rokon Uzzaman <19201103040@cse.bubt.edu.bd>
2026-01-07 20:01:09 +0600

    refactor: Remove Home and authentication/guest navigation links, and add `x-cloak` to the navigation container.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,10 +37,7 @@
 
                         <!-- Navigation Links -->
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex" x-cloak>
-                            <a href="{{ route('home') }}"
-                                class="inline-flex items-center px-1 pt-1 border-b-2 {{ request()->routeIs('home') ? 'border-red-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} text-sm font-medium leading-5 transition duration-150 ease-in-out">
-                                Home
-                            </a>
+
                             
                             <a href="{{ route('packages.index') }}"
                                 x-show="showNavLinks"
@@ -64,14 +61,7 @@
                         </div>
                     </div>
 
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                        @auth
-                            <a href="{{ route('dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endauth
-                    </div>
+
                 </div>
             </div>
         </nav>
